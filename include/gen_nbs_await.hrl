dfa9389521c193d3d2a8e86dcc015a39d25b56f9
Kirill Kinduk <evilbluebeaver@gmail.com>
2016-05-24 16:40:06 +0300

    redesign

--- include/gen_nbs_await.hrl
+++ include/gen_nbs_await.hrl
@@ -1,6 +1,7 @@
 -record(await, {master_ref :: reference(),
+                tag :: term(),
                 timer_ref :: reference() | undefined,
                 child_refs :: #{term() => reference()} | undefined}).
 
--type await() :: #{Tag :: term() => [#await{}] | #await{}}.
+-type await() :: #await{}.
 
